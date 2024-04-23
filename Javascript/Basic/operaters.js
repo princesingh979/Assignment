3b2cd066987db016e489c275b2fa930a339db3e5
princesingh979 <princepk979.pp@gmail.com>
2024-04-23 09:20:32 +0545

    updated

--- Javascript/Basic/operaters.js
+++ Javascript/Basic/operaters.js
@@ -1,16 +1,16 @@
-//Q1 create two constants, add them assign to a new constants
+// //Q1 create two constants, add them assign to a new constants
 
-const a = 10
-const b = '10'
-const c =a + b
-console.log(c) 
+// const a = 10
+// const b = '10'
+// const c =a + b
+// console.log(c) 
 
 
-//Check below code, output should be false by comparing. console.log the answer
-const a = 10
-const b = '10'
+// //Check below code, output should be false by comparing. console.log the answer
+// const a = 10
+// const b = '10'
 
-console.log(a===b)a
+// console.log(a===b)
 
 
 
