21337624e5e8dd2c89c7d6a62d10571c14d58be2
princesingh979 <princepk979.pp@gmail.com>
2024-04-23 08:58:47 +0545

    generate

--- Javascript/Basic/operaters.js
+++ Javascript/Basic/operaters.js
@@ -12,3 +12,18 @@ const b = '10'
 
 console.log(a===b)a
 
+
+
+
+// generate a random number between 0 and 5, if it is lesser than 3,
+//console.log "lesser than 3" else "Greater"
+
+// Generate a random number between 0 and 5
+const randomNumber = Math.floor(Math.random() * 6); // Random number between 0 (inclusive) and 6 (exclusive)
+
+// Check if the random number is less than 3
+if (randomNumber < 3) {
+    console.log("lesser than 3");
+} else {
+    console.log("Greater");
+}
