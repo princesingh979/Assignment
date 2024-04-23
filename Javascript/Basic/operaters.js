e247cbe7b104661c84d99205e10b0bc2b7f9da57
princesingh979 <princepk979.pp@gmail.com>
2024-04-23 08:39:16 +0545

    answer

---
+++ Javascript/Basic/operaters.js
@@ -0,0 +1,11 @@
+//Q1 create two constants, add them assign to a new constants
+
+const a = 10
+const b = '10'
+const c =a + b
+console.log(c) 
+
+
+//Check below code, output should be false by comparing. console.log the answer
+const a = 10
+const b = '10'
