3e0e52d0b47e863706851cafe3e1c0a8fa4b3aad
princesingh979 <princepk979.pp@gmail.com>
2024-05-07 01:00:26 +0545

    update

---
+++ reactjs/my-app/src/app/login/page.js
@@ -0,0 +1,24 @@
+import {Input,Button} from "@nextui-org/react";
+import React from 'react'
+import Link from "next/link";
+const login = () => {
+  return (
+    <div className="min-h-screen flex items-center flex justify-right bg-gray-500 py-12 px-4 sm:px-6 lg:px-8 text box-border">
+    <div className="border border-purple-300 w-96 h-95 ">
+     <div className='text-center text-5xl p-4 font-serif '>
+     <h1>Facebook</h1></div>
+    <div className="  p-2 m-2">
+   <Input type="email" placeholder="Enter your user or email" /></div>
+   <div className="  p-2 m-2 ">
+   <Input type="password"  placeholder="Enter your password" /></div>
+   <div className='text-center  decoration-auto'>
+   <Button color="primary">Login </Button></div>
+   <div  className='text-center  decoration-auto'>
+   <Button color="primary"> <Link href="register">Register</Link></Button></div>
+
+ </div>
+ </div>
+  )
+}
+
+export default login
